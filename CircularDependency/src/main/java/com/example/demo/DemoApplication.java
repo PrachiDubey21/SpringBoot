5c44e9dcf81370688853062026e5c8228409f8f8
Prachi Dubey <006prachi@gmail.com>
2026-09-23 22:28:28 +0530

    Circular Dependency

--- CircularDependency/src/main/java/com/example/demo/DemoApplication.java
+++ CircularDependency/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ApplicationContext context =
+				SpringApplication.run(DemoApplication.class, args);
+
+		OrderService order = context.getBean(OrderService.class);
+
+		order.placeOrder();
 	}
 
 }
---
+++ CircularDependency/src/main/java/com/example/demo/OrderService.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+
+	@Autowired
+	private PaymentService paymentService;
+
+	// public OrderService(PaymentService paymentService) {
+	// 	this.paymentService = paymentService;
+	// }
+
+	public void placeOrder() {
+
+		paymentService.pay();
+
+		// call here
+		getOrderDetails();
+
+		System.out.println("Order placed");
+	}
+
+	public void getOrderDetails() {
+		System.out.println("Order Details");
+	}
+}
